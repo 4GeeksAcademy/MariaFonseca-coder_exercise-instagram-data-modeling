59c2a6a0e052a668af65d13dc890aca5c733d767
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-11-30 03:01:17 +0000

    Tablas completas de Instagram (algunas de las reales existentes)

--- src/models.py
+++ src/models.py
@@ -7,41 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    user_from_id = Column(Integer)
-    user_to_id = Column(Integer)
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(20))
+    username = Column(String(20), unique=True)
     firstname = Column(String(20))
     lastname = Column(String(20))
     email = Column(String(25), nullable=False)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     type = Column(Enum('image', 'video', 'audio', name='media_types'))
     url = Column(String(250))
-    post_id = Column(Integer)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    post = relationship(Post)
 
-class Post(Base):
-    __tablename__ = 'post'
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
+    comment_text = Column(String(100))
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    post = relationship(Post)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    author = relationship(User)
 
     def to_dict(self):
         return {}
